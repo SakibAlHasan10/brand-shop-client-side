07793cecb61d79724f09bf350f168347ffa223d2
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-17 21:58:54 +0600

    add navbar section

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,99 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
+          <img src="/tidal-wave.png" alt="" className="h-16 w-40"/>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <NavLink
+                to={`/home`}
+                className={`${({ isActive, isPending }) =>
+                  isActive ? "active" : isPending ? "pending" : ""}
+            `}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`/add_product`}
+                className={`${({ isActive, isPending }) =>
+                  isActive ? "active" : isPending ? "pending" : ""}
+            `}
+              >
+                Add Product
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`/my_cart`}
+                className={`${({ isActive, isPending }) =>
+                  isActive ? "active" : isPending ? "pending" : ""}
+            `}
+              >
+                My Cart
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <NavLink
+            to={`/login`}
+            className={`${({ isActive, isPending }) =>
+              isActive ? "active" : isPending ? "pending" : ""}
+            btn-sm`}
+          >
+            LogIn
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/Pages/RootPage/Root.jsx
+++ src/Pages/RootPage/Root.jsx
@@ -1,7 +1,9 @@
+import Navbar from "../../Components/Navbar/Navbar";
 
 const Root = () => {
     return (
         <div>
+            <Navbar></Navbar>
             Root
         </div>
     );
