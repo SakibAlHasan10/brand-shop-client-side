e275f4dd090f2d942bb837361482aea5532461c8
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-18 16:09:02 +0600

    add slider img

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,8 +2,13 @@ import Slider from "../../Sheare/Slider/Slider";
 
 const Banner = () => {
     return (
-        <div className="my-5">
+        <div className="my-5 h-[70vh] ">
+            <div className="flex gap-5 ">
+                <div className="w-3/4">
             <Slider></Slider>
+                </div>
+            <img src="https://kitpro.site/lawncare/wp-content/uploads/sites/193/2023/09/image-2-1.jpg" alt="" className="w-1/4 h-[70vh] rounded-lg" />
+            </div>
         </div>
     );
 };
--- src/Sheare/Slider/Slider.jsx
+++ src/Sheare/Slider/Slider.jsx
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination,autoplay, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import "swiper/css";
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -8,17 +8,21 @@ const Slider = () => {
   return (
     <div>
       <Swiper
-        modules={[Navigation, autoplay, Pagination, Scrollbar, A11y]}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={50}
         slidesPerView={1}
         navigation
+        height={50}
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log('slide change')}
       >
-        <SwiperSlide><img src="https://kitpro.site/lawncare/wp-content/uploads/sites/193/2023/09/young-woman-gardener-caring-plants-treating-flower-2023-01-31-23-19-44-utc-2.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://kitpro.site/lawncare/wp-content/uploads/sites/193/2023/09/young-woman-gardener-caring-plants-treating-flower-2023-01-31-23-19-44-utc-1.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide>
+            <img src="https://kitpro.site/lawncare/wp-content/uploads/sites/193/2023/09/young-woman-gardener-caring-plants-treating-flower-2023-01-31-23-19-44-utc-2.jpg" alt="" className="h-[70vh] w-full rounded-lg" />
+            </SwiperSlide>
+        <SwiperSlide>
+            <img src="https://kitpro.site/lawncare/wp-content/uploads/sites/193/2023/09/young-woman-gardener-caring-plants-treating-flower-2023-01-31-23-19-44-utc-1.jpg" alt="" className="h-[70vh] w-full rounded-lg"/></SwiperSlide>
       </Swiper>
     </div>
   );
