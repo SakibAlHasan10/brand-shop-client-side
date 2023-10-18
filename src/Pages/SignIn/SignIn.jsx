c4eef08e80f690a9c0272fb0e14a3e44c3d12e3b
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-18 13:38:58 +0600

    setup signin page

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,10 +1,66 @@
+import { useState } from "react";
 
 const SignIn = () => {
-    return (
-        <div>
-            signin
+    const [errorText, setErrorText] = useState('')
+  const handleSignIn =(e)=>{
+    e.preventDefault()
+    setErrorText('')
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    
+    console.log(email, password)
+  }
+  return (
+    <div>
+      <div className="hero min-h-screen">
+        <div className="hero-content w-full flex-col">
+          <div className="text-center">
+            <h1 className="text-3xl font-bold">Sign In now!</h1>
+            
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
+            <form onSubmit={handleSignIn} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input w-full input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+                <p className="text-red-600 text-lg mt-2 font-semibold">{errorText}</p>
+              </div>
+              <div className="form-control mt-2">
+                <button type="submit" className="btn btn-primary">Sign In</button>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
