11b5ef1abff6ac5817a1158496a6f58ea5328503
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-18 19:20:46 +0600

    added 404 page image and button

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div className="text-center text-5xl">
-            Oops!!
+        <div className="text-center mt-20 text-5xl w-full ">
+            <img src="https://kitpro.site/lawncare/wp-content/uploads/sites/193/2023/10/404-01-800x322.png" alt="" className="w-1/2 mx-auto" />
+            <h2 className="text-center text-green-700 text-4xl lg:text-5xl my-4 font-bold">Oops! Page Not Found</h2>
+            <Link to={'/'}>
+            <button className="btn btn-neutral">Go home</button>
+            </Link>
         </div>
     );
 };
