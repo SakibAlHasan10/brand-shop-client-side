0e9a10167c9c2dc07b510810d458eb5f2a1091ec
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-19 10:21:09 +0600

    add successfull massege before add product

--- src/Pages/AddProduct/AddProduct.jsx
+++ src/Pages/AddProduct/AddProduct.jsx
@@ -1,30 +1,46 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const AddProduct = () => {
-    const handleAddProduct = (e)=>{
-        e.preventDefault()
-        const form = e.target;
-        const name = form.name.value;
-        const brand = form.brand.value;
-        const price = form.price.value;
-        const category = form.category.value;
-        const rating = form.rating.value;
-        const description = form.description.value;
-        const photo = form.photo.value;
-        const product ={name, brand, price, category, rating, description, photo}
-        fetch('http://localhost:5000/products',{
-          method:"POST",
-          headers: {
-            "content-type": 'application/json'
-          },
-          body: JSON.stringify(product)
-        })
-        .then(res =>res.json())
-        .then(data=>{
-          console.log(data)
-        })
-        // console.log(name, brand, price, category, rating, description, photo)
-    }
+  const handleAddProduct = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const brand = form.brand.value;
+    const price = form.price.value;
+    const category = form.category.value;
+    const rating = form.rating.value;
+    const description = form.description.value;
+    const photo = form.photo.value;
+    const product = {
+      name,
+      brand,
+      price,
+      category,
+      rating,
+      description,
+      photo,
+    };
+    fetch("http://localhost:5000/products", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(product),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          toast.success("product added successful", {
+            position: toast.POSITION.TOP_RIGHT,
+          });
+        }
+        console.log(data);
+      });
+    // console.log(name, brand, price, category, rating, description, photo)
+  };
   return (
     <div className="max-w-screen-lg mx-auto mb-10 px-8">
+      <ToastContainer />
       <h2 className="text-3xl font-semibold mt-14 mb-10">Add Product</h2>
       <form onSubmit={handleAddProduct} className="border p-5 rounded-lg">
         <div className="flex gap-6 justify-between">
@@ -44,10 +60,12 @@ const AddProduct = () => {
             <label className="label">
               <span className="label-text">Brand Name</span>
             </label>
-            <select className="select select-bordered w-full" name="brand" defaultValue="default">
-              <option >
-                Apple
-              </option>
+            <select
+              className="select select-bordered w-full"
+              name="brand"
+              defaultValue="default"
+            >
+              <option>Apple</option>
               <option>Samsung</option>
               <option>Sony</option>
               <option>Sony</option>
@@ -73,10 +91,12 @@ const AddProduct = () => {
             <label className="label">
               <span className="label-text">Category</span>
             </label>
-            <select className="select select-bordered w-full" name="category" defaultValue="default">
-              <option >
-                Phone
-              </option>
+            <select
+              className="select select-bordered w-full"
+              name="category"
+              defaultValue="default"
+            >
+              <option>Phone</option>
               <option>Laptop</option>
               <option>MacBook</option>
             </select>
@@ -109,18 +129,20 @@ const AddProduct = () => {
           </div>
         </div>
         <div className="form-control w-full">
-            <label className="label">
-              <span className="label-text">Photo URL</span>
-            </label>
-            <input
-              type="text"
-              name="photo"
-              placeholder="photo URL"
-              className="input w-full  input-bordered"
-              required
-            />
-          </div>
-        <button type="submit" className="btn w-full mt-5 btn-outline">Add Product</button>
+          <label className="label">
+            <span className="label-text">Photo URL</span>
+          </label>
+          <input
+            type="text"
+            name="photo"
+            placeholder="photo URL"
+            className="input w-full  input-bordered"
+            required
+          />
+        </div>
+        <button type="submit" className="btn w-full mt-5 btn-outline">
+          Add Product
+        </button>
       </form>
     </div>
   );
